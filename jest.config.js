407d9c8c8e481f2594c2aa57c5410a55cb4b1e24
Nicholas Kalpakis <nkalpakis21@gmail.com>
2025-09-14 09:13:42 -0400

    Fix Jest configuration and prepare for v1.0.0 release

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest'
   },
-  moduleNameMapping: {
+  moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   }
 };
